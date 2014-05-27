aaa80c2ea8aff1b5a9a56e0bf2731fdb259ba16e
Julio Bitar <assassinpoint@gmail.com>
2014-05-26 18:46:31 -0600

    solucion errores shim

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,6 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
-
   it { should respond_to(:authenticate) }
 
 
